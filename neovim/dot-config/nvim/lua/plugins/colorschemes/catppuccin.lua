c2d7958b82ea00c570b022669b019f20fc7c5745
xemanue <josemanuel@garciacazorla.es>
2025-11-09 13:40:25 +0100

    pre-formatting backup

--- neovim/dot-config/nvim/lua/plugins/colorschemes/catppuccin.lua
+++ neovim/dot-config/nvim/lua/plugins/colorschemes/catppuccin.lua
@@ -5,7 +5,7 @@ return {
         priority = 1000,
         config = function()
             require("catppuccin").setup({
-                flavour = "frappe",
+                flavour = "mocha",
                 no_italic = true,
                 no_bold = true,
                 no_underline = true,
